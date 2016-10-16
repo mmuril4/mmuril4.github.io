ed674784b030bb9e4ff17e6ebfcf84b574e8488a
mmuril4 <mmuril4@gmail.com>
2016-10-15 21:43:57 -0500

    Updated steps 3 and 4 added steps 5 and 6

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // Initialize Firebase
 
-var firebaseURL = "https://www.gstatic.com/firebasejs/3.3.0/firebase.js";
+//var firebaseURL = "https://www.gstatic.com/firebasejs/3.3.0/firebase.js";
+
 //Step 1
-  var config = {
-    apiKey: "AIzaSyAf82fQwO7oErwFf_jZYRP3dhQO0Q0VhU8",
-    authDomain: "reservation-site-62a8f.firebaseapp.com",
-    databaseURL: "https://reservation-site-62a8f.firebaseio.com",
-    storageBucket: "reservation-site-62a8f.appspot.com",
-    messagingSenderId: "732766382169"
+var config = {
+    apiKey: "AIzaSyC7ip5fD3CO4d1JoF9spK_UQTzNubEkfqc",
+    authDomain: "final-project-ffe96.firebaseapp.com",
+    databaseURL: "https://final-project-ffe96.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "498770179925"
   };
+
   firebase.initializeApp(config);
 
 
 //Connect to Database
 var database = firebase.database();
-
-
 
 
 //Step 2
@@ -26,11 +26,11 @@ var reservationData = {};
 //Step 3
 $('.reservation-day li').on('click', function () {
 	
-	var day = $(this).val();
+	//var day = $(this).val();
 
 	reservationData.day = $(this).text();
 
-	alert($(this).text());
+	//alert($(this).text());
 
 });
 
@@ -42,13 +42,14 @@ $('form').on('submit', function (e) {
 	e.preventDefault();
 
 	//grab user's comment from input
-	var name = $('.reservation-name').val();
+	//var name = $('.reservation-name').val();
 
 	//clear users input
-	$('.reservation-name').val('');
+	//$('.reservation-name').val('');
 
 	//add input to reservationData object
-	reservationData.name = $(this).text();
+	//reservationData.name = $('.reservation-name').text();
+	reservationData.name = $('.reservation-name').val();
 
 	//alert(name);
 
@@ -59,18 +60,48 @@ $('form').on('submit', function (e) {
 	var reservationDataReference = database.ref('reservationsMade');
 
 	//use set method to save data to the reservationsMade
-	reservationDataReference.push({
-		comment: day,
-		comment: name
-	});
+	//reservationDataReference.push({
+	//	comment: day,
+	//	comment: name
+	reservationDataReference.push(reservationDataReference);
+});
 
 
 //Step 6
 function getReservations () {
 
+	//use reference to database to listen for changes in reservation data
+	database.ref('reservationsMade').on('value', function (results) {
+
+		//get all reservations stored in the results we received back from Firebase
+		var allReservations = results.val();
+
+		//remove all list reservations from DOM before appending list reservations
+		$('reservations').empty();
+
+	});
+
+	//iterate through all reservations coming from db
+	for (var reservation in allReservations) {
+
+		//Create an object literal with the data we'll pass to Handlebars
+		var context = {
+			name: allReservations[reservation].name,
+			day: allReservations[reservation].day,
+		};
+
+	var source = $("#reservation-template").html();
+	var template = Handlebars.compile(source);
+	var reservationListItem = template(context);
+	$('.reservations').append(reservationListItem);
+	}
+
+
 }
 
-});
+//Step 7
+
+
 
 
 
