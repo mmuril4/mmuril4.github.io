1d347dbd322d3ab2bfc34b1f68c4b0472de48e05
mmuril4 <mmuril4@gmail.com>
2016-10-16 12:06:56 -0500

    Step 6 and 7 added: google map and marker

--- js/main.js
+++ js/main.js
@@ -101,6 +101,21 @@ function getReservations () {
 
 //Step 7
 
+      function initMap() {
+        //var uluru = {lat: 40.8054491, lng: -73.9654415};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.8054491, lng: -73.9654415};
+          zoom: 10,
+          scrollwheel: false,
+        });
+
+        var marker = new google.maps.Marker({
+          position: {lat: 40.8054491, lng: -73.9654415};,
+          map: map,
+          title: 'Monks Café'
+        });
+      }
+
 
 
 
