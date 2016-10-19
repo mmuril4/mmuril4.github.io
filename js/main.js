e6a3b71e51673e0453893785d3c3603554e35309
mmuril4 <mmuril4@gmail.com>
2016-10-18 21:55:03 -0500

    unit 10 final project

--- js/main.js
+++ js/main.js
@@ -1,153 +1,80 @@
 // Initialize Firebase
-
-//var firebaseURL = "https://www.gstatic.com/firebasejs/3.3.0/firebase.js";
-
 //Step 1
 var config = {
-    apiKey: "AIzaSyC7ip5fD3CO4d1JoF9spK_UQTzNubEkfqc",
-    authDomain: "final-project-ffe96.firebaseapp.com",
-    databaseURL: "https://final-project-ffe96.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "498770179925"
-  };
+	apiKey: "AIzaSyC7ip5fD3CO4d1JoF9spK_UQTzNubEkfqc",
+	authDomain: "final-project-ffe96.firebaseapp.com",
+	databaseURL: "https://final-project-ffe96.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "498770179925"
+};
 
-  firebase.initializeApp(config);
-
-
+firebase.initializeApp(config);
 //Connect to Database
 var database = firebase.database();
-
 
 //Step 2
 var reservationData = {};
 
-
-
 //Step 3
 $('.reservation-day li').on('click', function () {
-	
-	//var day = $(this).val();
-
 	reservationData.day = $(this).text();
-
-	//alert($(this).text());
-
 });
-
-
-
-//Step 4
 
 $('.reservations').on('submit', function (e) {
 	e.preventDefault();
-
-	//grab user's comment from input
-	//var name = $('.reservation-name').val();
-
-	//clear users input
-	//$('.reservation-name').val('');
-
 	//add input to reservationData object
-	//reservationData.name = $('.reservation-name').text();
 	reservationData.name = $('.reservation-name').val();
 
-	//alert(name);
-
-
 //Step 5
-
 	//section for reservationData (U9L4S35)
 	var reservationDataReference = database.ref('reservationsMade');
-
 	//use set method to save data to the reservationsMade
 	reservationDataReference.push(reservationData);
-
 });
-
 
 //Step 6
 function getReservations () {
-
 	//use reference to database to listen for changes in reservation data
 	database.ref('reservationsMade').on('value', function (results) {
-
 		//get all reservations stored in the results we received back from Firebase
 		var allReservations = results.val();
-
 		//remove all list reservations from DOM before appending list reservations
 		$('reservationsMade').empty();
-
 	//iterate through all reservations coming from db
 	for (var reservation in allReservations) {
-
 		//Create an object literal with the data we'll pass to Handlebars
 		var context = {
 			name: allReservations[reservation].name,
 			day: allReservations[reservation].day,
 			reservationId: reservation
 		};
-
-	var source = $("#reservation-template").html();
-	var template = Handlebars.compile(source);
-	var reservationListItem = template(context);
-	$('.reservation-list').empty(reservationListItem);
-	$('.reservation-list').append(reservationListItem);
+		var source = $("#reservation-template").html();
+		var template = Handlebars.compile(source);
+		var reservationListItem = template(context);
+		$('.reservation-list').empty(reservationListItem);
+		$('.reservation-list').append(reservationListItem);
 	}
-
+});
+}
+//Step 7-9
+function initMap() {
+	var uluru = {lat: 40.8054491, lng: -73.9654415};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: uluru,
+		zoom: 10,
+		scrollwheel: false
 	});
-
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+		title: 'Monks Café'q
+	});
 }
 
-
-//Step 7-9
-
-      function initMap() {
-      	var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: uluru,
-          zoom: 10,
-          scrollwheel: false
-        });
-
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-          title: 'Monks Café'
-        });
-      }
-
-
-
 //Bonuses
-
-
-
-// $('.reservations').on('submit', function (e) {
-
-//     e.preventDefault();
-
-//     var itemText = $('.reservation-name').find('input').val();
-
-//     var newListItem = '<input type="checkbox"><span class="item">' + itemText + '</span><a href="#" class="edit">Edit</a><a href="#" class="remove">Remove</a>';
-
-//     $(this).parent().html(newListItem);    
-// });
-
-
-$('#deleteItems').on('click', function () {
-
-    $('.reservation-list').empty();
-
+$('#deleteButton').on('click', function () {
+	//$('.reservationtest[data-id="reservationId"]')
+	$('.reservation-list').empty();
 });
 
-
-
 getReservations();
-
-
-
-
-
-
-
-
